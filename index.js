23ec5a6ccd1532a0cc7b26854865898d3a484deb
tkamanda <toelokemwa@gmail.com>
2024-07-24 04:25:32 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
 
-/*
+function createEmployeeRecords(employeeRowData) {
+    return employeeRowData.map(row => createEmployeeRecord(row));
+
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, time] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(time)
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, time] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour:parseInt(time)
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date);
+    let timeOut = this.timeOutEvents.find(e => e.date === date);
+
+    if (timeIn && timeOut) {
+        return (timeOut.hour - timeIn.hour) /100;
+    }
+    return 0;
+}
+
+function wagesEarnedOnDate(date) {
+    let hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total,employee) => {
+        return total + allWagesFor.call(employee);
+    }, 0);
+}
+    /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
